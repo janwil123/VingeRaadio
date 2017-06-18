8561444f35cdbd9bdc6bd5c92bbaa35566a61272
janwil123 <janwil@hot.ee>
2017-06-18 16:53:26 +0300

    Esialgne raadio tarkvarafail

---
+++ VingeRaadio.ino
@@ -0,0 +1,106 @@
+///
+/// \file  TestTEA5767.ino
+/// \brief An Arduino sketch to operate a TEA5767 chip based radio using the Radio library.
+///
+/// \author Matthias Hertel, http://www.mathertel.de
+/// \copyright Copyright (c) 2014 by Matthias Hertel.\n
+/// This work is licensed under a BSD style license.\n
+/// See http://www.mathertel.de/License.aspx
+///
+/// \details
+/// This sketch implements a "as simple as possible" radio without any possibility to modify the settings after initializing the chip.\n
+/// The radio chip is initialized and setup to a fixed band and frequency. These settings can be changed by modifying the 
+/// FIX_BAND and FIX_STATION definitions. 
+///
+/// Open the Serial console with 57600 baud to see the current radio information.
+///
+/// Wiring
+/// ------ 
+/// Arduino port | Si4703 signal
+/// ------------ | ---------------
+///     3.3V | VCC
+///      GND | GND
+///       A5 | SCLK
+///       A4 | SDIO
+///       D2 | RST
+///
+/// More documentation and source code is available at http://www.mathertel.de/Arduino
+///
+/// History:
+/// --------
+/// * 15.09.2014 created.
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <radio.h>
+#include <TEA5767.h>
+
+/// The band that will be tuned by this sketch is FM.
+#define FIX_BAND RADIO_BAND_FM
+
+/// The station that will be tuned by this sketch is 89.30 MHz.
+#define FIX_STATION 9940
+
+TEA5767 radio;    // Create an instance of Class for Si4703 Chip
+
+uint8_t test1;
+byte test2;
+
+const int buttonPin = 2;     // the number of the pushbutton pin
+
+int buttonState = 0;         // variable for reading the pushbutton status
+
+int sagedused[4] = {8850, 9440, 9520, 9940};
+
+int jaam = 0;
+
+/// Setup a FM only radio configuration
+/// with some debugging on the Serial port
+void setup() {
+  // open the Serial port
+  Serial.begin(57600);
+  Serial.println("Radio...");
+  delay(200);
+
+  // Initialize the Radio 
+  radio.init();
+
+  // Enable information to the Serial port
+  radio.debugEnable();
+
+  // HERE: adjust the frequency to a local sender
+  radio.setBandFrequency(FIX_BAND, FIX_STATION); // hr3 nearby Frankfurt in Germany
+  radio.setVolume(2);
+  radio.setMono(false);
+
+    // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+} // setup
+
+
+/// show the current chip data every 3 seconds.
+void loop() {
+  char s[12];
+  radio.formatFrequency(s, sizeof(s));
+  Serial.print("Station:"); 
+  Serial.println(s);
+  
+  Serial.print("Radio:"); 
+  radio.debugRadioInfo();
+  
+  Serial.print("Audio:"); 
+  radio.debugAudioInfo();
+
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    // button pressed
+    Serial.print("Button pressed");
+    jaam = (jaam + 1) % 4; 
+    radio.setBandFrequency(FIX_BAND, sagedused[jaam]);
+  } 
+
+  delay(3000);
+} // loop
+
+// End.
+
