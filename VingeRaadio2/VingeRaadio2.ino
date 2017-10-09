26f072c6b518dcec185809323f4dd3f02afb71b2
Jan Willemson <janwil@hot.ee>
2017-10-09 22:31:22 +0300

    Lisasime nupufunktsionaalsuse

--- VingeRaadio2/VingeRaadio2.ino
+++ VingeRaadio2/VingeRaadio2.ino
@@ -22,7 +22,41 @@ char rdsBuffer[10];
 
 // Pins for the bush buttons
 
-const int button1 = 4, button2 = 5, button4 = 6, button8 = 7;
+const int button1Pin = 4;
+const int button2Pin = 5;
+const int button4Pin = 6;
+const int button8Pin = 7;// the number of the pushbutton pin
+
+int button1State = 0;
+int button2State = 0;
+int button4State = 0;
+int button8State = 0;
+
+int pinsState[4] = {0,0,0,0};
+
+bool pinsPressed = false;
+
+int sagedused[9] = { 1067,
+                     1036, 
+                     1057, 
+                     1047, 
+                     994, 
+                     885, 
+                     1002, 
+                     952, 
+                     986};
+                     
+char* jaamanimed[9]={"Vikerraadio     ", 
+                     "Raadio 2        ", 
+                     "Klassikaraadio  ", 
+                     "Ring FM         ", 
+                     "Star FM         ",
+                     "Raadio Elmar    ", 
+                     "Raadio Kuku     ",
+                     "Sky Plus        ",
+                     "Retro FM        "};
+
+int jaam = 0;
 
 void setup()
 {
@@ -46,20 +80,44 @@ void setup()
   lcd.setCursor(0, 0);
   lcd.print("Hello, world!");
 
-  pinMode(button1, INPUT);
-  digitalWrite(button1, HIGH);
-  pinMode(button2, INPUT);
-  digitalWrite(button2, HIGH);
-  pinMode(button4, INPUT);
-  digitalWrite(button4, HIGH);
-  pinMode(button8, INPUT);
-  digitalWrite(button8, HIGH);
+  pinMode(button1Pin, INPUT);
+  digitalWrite(button1Pin, HIGH);
+  pinMode(button2Pin, INPUT);
+  digitalWrite(button2Pin, HIGH);
+  pinMode(button4Pin, INPUT);
+  digitalWrite(button4Pin, HIGH);
+  pinMode(button8Pin, INPUT);
+  digitalWrite(button8Pin, HIGH);
 }
 
 void loop()
 { 
-  
+  button1State = digitalRead(button1Pin);
+  button2State = digitalRead(button2Pin);
+  button4State = digitalRead(button4Pin);
+  button8State = digitalRead(button8Pin);
 
+  Serial.println(button1State);
+  
+  if ((button1State==LOW) || (button2State==LOW) || (button4State==LOW) || (button8State==LOW)){
+    Serial.println("Midagi vajutati");
+    pinsPressed = true;
+    pinsState[0] = (pinsState[0] || (button1State==LOW));
+    pinsState[1] = (pinsState[1] || (button2State==LOW));
+    pinsState[2] = (pinsState[2] || (button4State==LOW));
+    pinsState[3] = (pinsState[3] || (button8State==LOW));
+  }
+  else {
+    if (pinsPressed){
+      jaam = pinsState[0] + 2*pinsState[1] + 4*pinsState[2] + 8*pinsState[3]-1;
+      Serial.println(jaam);
+      pinsState[0] = 0;
+      pinsState[1] = 0;
+      pinsState[2] = 0;
+      pinsState[3] = 0;
+      pinsPressed = false;
+    }
+  }
   
   if (Serial.available())
   {
